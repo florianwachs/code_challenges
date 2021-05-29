d6ec120d33d8541055fa9d1f490309272c9bd1a0
Florian Wachs <Florian.Wachs@halvotec.de>
2021-05-29 15:44:34 +0200

    feat:kata

---
+++ src/csharp/CodeChallenges/001_ConvertToCamelCase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CodeChallenges
+{
+    public class ConvertToCamelCase_001
+    {
+        [Theory]
+        [InlineData("the-stealth-warrior", "theStealthWarrior")]
+        [InlineData("The_Stealth_Warrior", "TheStealthWarrior")]
+        public void ConvertToCamelCase(string input, string expected)
+        {
+            Assert.Equal(expected, ToCamelCase(input));
+        }
+
+        public string ToCamelCase(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return input;
+            }
+
+            var splits = input.Split('-', '_');
+            return string.Join("", splits.Take(1).Concat(splits.Skip(1).Select(Capitalize)));
+
+        }
+
+        private string Capitalize(string input) => Char.ToUpper(input[0]) + input[1..];
+    }
+}
