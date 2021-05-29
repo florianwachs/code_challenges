9201b8e94f7937224d6603b8a09f851a63ab3bba
Florian Wachs <Florian.Wachs@halvotec.de>
2021-05-29 16:11:07 +0200

    feat: kata

---
+++ src/csharp/CodeChallenges/003_SumofDigits.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CodeChallenges
+{
+    public class SumofDigits_003
+    {
+        [Theory]
+        [InlineData(16, 7)]
+        [InlineData(942, 6)]
+        [InlineData(132189, 6)]
+        [InlineData(493193, 2)]
+        public void CanSumOfDigits(int input, int expected)
+        {
+            Assert.Equal(expected, SumofDigits(input));
+        }
+
+        private int SumofDigits(int input) =>
+            input switch
+            {
+                < 0 => throw new ArgumentException(),
+                < 10 => input,
+                _ => SumofDigits(input.ToString().Select(c => int.Parse(c.ToString())).Sum())
+            };
+    }
+}
