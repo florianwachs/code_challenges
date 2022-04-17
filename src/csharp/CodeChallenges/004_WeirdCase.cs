44d1734c813724f6a0565f6b4ca30d046b7f084a
Florian Wachs <2216547+florianwachs@users.noreply.github.com>
2022-04-17 23:33:42 +0200

    weirdcase

---
+++ src/csharp/CodeChallenges/004_WeirdCase.cs
@@ -0,0 +1,27 @@
+﻿namespace CodeChallenges;
+
+public class WeirdCase_004
+{
+    [Theory]
+    [InlineData("String", "StRiNg")]
+    [InlineData("Weird string case", "WeIrD StRiNg CaSe")]
+    public void CheckToWeirdCase(string input, string expected)
+    {
+        Assert.Equal(expected, ToWeirdCase(input));
+    }
+
+    public string ToWeirdCase(string input)
+    {
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            return string.Empty;
+        }
+
+        return string.Join("", input.Select((c, idx) => (c, idx % 2) switch
+         {
+             (' ', _) => ' ',
+             (_, 0) => Char.ToUpper(c),
+             _ => Char.ToLower(c),
+         }));
+    }
+}
