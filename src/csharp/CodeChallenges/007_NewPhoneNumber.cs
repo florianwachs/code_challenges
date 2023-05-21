ed8754e5320057c3541e77f39245a3469d94f413
Florian Wachs <florian.wachs@live.de>
2023-05-21 11:57:42 +0200

    Phone number challenge

---
+++ src/csharp/CodeChallenges/007_NewPhoneNumber.cs
@@ -0,0 +1,12 @@
+﻿namespace CodeChallenges;
+
+public class NewPhoneNumber_007
+{
+    [Fact]
+    public void CheckPhoneNumber()
+    {
+        Assert.Equal("(123) 456-7890", CreatePhoneNumber(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 }));
+    }
+
+    public string CreatePhoneNumber(int[] numbers) => $"({string.Join("", numbers[0..3])}) {string.Join("", numbers[3..6])}-{string.Join("", numbers[6..])}";
+}
